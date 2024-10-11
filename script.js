3122649e8a75f34082c9fb1bea6691ca10ae7127
Kellarosaa <146978293+Kellarosaa@users.noreply.github.com>
2024-10-10 21:08:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,47 +2,64 @@ document.getElementById('addTextBox').addEventListener('click', function() {
     const textBox = document.createElement('div');
     textBox.className = 'text-box';
     textBox.contentEditable = true;
-    textBox.innerText = 'Edit me!';
     textBox.style.left = '50px';
     textBox.style.top = '50px';
+    textBox.innerText = 'Edit me!';
+    textBox.onmousedown = function(e) {
+        let shiftX = e.clientX - textBox.getBoundingClientRect().left;
+        let shiftY = e.clientY - textBox.getBoundingClientRect().top;
+
+        function moveAt(pageX, pageY) {
+            textBox.style.left = pageX - shiftX + 'px';
+            textBox.style.top = pageY - shiftY + 'px';
+        }
+
+        function onMouseMove(e) {
+            moveAt(e.pageX, e.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        textBox.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            textBox.onmouseup = null;
+        };
+    };
+
     document.getElementById('canvas').appendChild(textBox);
-    makeDraggable(textBox);
 });
 
 document.getElementById('uploadImage').addEventListener('change', function(event) {
     const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = function(e) {
-        const img = document.createElement('img');
-        img.src = e.target.result;
-        img.className = 'image-box';
-        img.style.width = '100px';
-        img.style.height = '100px';
-        img.style.left = '100px';
-        img.style.top = '100px';
-        document.getElementById('canvas').appendChild(img);
-        makeDraggable(img);
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(file);
+    img.style.left = '100px';
+    img.style.top = '100px';
+    img.onload = function() {
+        URL.revokeObjectURL(img.src);
     };
-    reader.readAsDataURL(file);
-});
+    img.onmousedown = function(e) {
+        let shiftX = e.clientX - img.getBoundingClientRect().left;
+        let shiftY = e.clientY - img.getBoundingClientRect().top;
 
-function makeDraggable(element) {
-    let offsetX, offsetY;
-    element.onmousedown = function(e) {
-        offsetX = e.clientX - element.getBoundingClientRect().left;
-        offsetY = e.clientY - element.getBoundingClientRect().top;
-        document.onmousemove = function(e) {
-            element.style.left = (e.clientX - offsetX) + 'px';
-            element.style.top = (e.clientY - offsetY) + 'px';
-        };
-        document.onmouseup = function() {
-            document.onmousemove = null;
-            document.onmouseup = null;
+        function moveAt(pageX, pageY) {
+            img.style.left = pageX - shiftX + 'px';
+            img.style.top = pageY - shiftY + 'px';
+        }
+
+        function onMouseMove(e) {
+            moveAt(e.pageX, e.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        img.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            img.onmouseup = null;
         };
     };
-}
 
-document.getElementById('downloadPDF').addEventListener('click', function() {
-    // PDF download functionality to be implemented
-    alert('PDF download functionality is not yet implemented.');
+    document.getElementById('canvas').appendChild(img);
 });
+
+// Functionality to download as PDF can be implemented using libraries like jsPDF
